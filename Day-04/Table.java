5c54fb88a03fd4c7a8dc4c9873b8f7803b76e346
Anurag <anuragxjangir@gmail.com>
2026-09-18 23:14:32 +0530

    Add Table

---
+++ Day-04/Table.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Table {
+    public static void main(String[] args) {
+        int num = 0;
+        int table = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("kiski table chahiye : ");
+        num = scanner.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+
+            table = num * i;
+
+            System.out.println(table);
+
+        scanner.close();
+    }
+}
+}
